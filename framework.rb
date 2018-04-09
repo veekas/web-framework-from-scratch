dc850c49a2dec23c8f4498093a7d1e8df9c8d2dd
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2018-04-08 23:02:20 -0400

    feat(framework): specify root and user routes

--- framework.rb
+++ framework.rb
@@ -1,5 +1,24 @@
 class App
+  def initialize(&block)
+    @routes = RouteTable.new(block)
+  end
+
   def call(env)
     [200, {}, ["200 OK", "Hash of HTTP headers", "Enumerable of stringable objects such as 'Hello World!'"]]
+  end
+
+  class RouteTable
+    def initialize(block)
+      @routes = []
+      instance_eval(&block)
+    end
+
+    def get(route_spec, &block)
+      @routes << Route.new(route_spec, block)
+      p @routes
+    end
+  end
+
+  class Route < Struct.new(:route_spec, :block)
   end
 end
--- main.rb
+++ main.rb
@@ -1,3 +1,11 @@
 require_relative 'framework'
 
-APP = App.new
+APP = App.new do
+  get '/' do
+    "this is the root"
+  end
+
+  get 'users/:username' do
+    'this is the user'
+  end
+end
